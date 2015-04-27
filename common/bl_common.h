b8a45ea81e07b41b9bbb126eb2ab3e7dbe2a82e6
hnuyx <365159110@qq.com>
2015-04-27 13:37:06 +0800

    create bl_common.h
    common data type

---
+++ common/bl_common.h
@@ -0,0 +1,31 @@
+/* File name: bl_common.h
+ *
+ * Description: base library common head file
+ *
+ * Create by henuyx on 2015-04-27
+ *
+ */
+
+#ifndef __BL_COMMON_H__
+#define __BL_COMMON_H__
+
+/*******************INCLUDE*******************************/
+
+/*******************GLOBAL********************************/
+
+namespace BL_NS
+{
+
+typedef unsigned long long  uint64;
+typedef unsigned int  uint32;
+typedef unsigned short uint16;
+typedef unsigned char uint8;
+
+typedef long long  int64;
+typedef int  int32;
+typedef short int16;
+typedef char int8;
+
+}
+
+/*******************EOF***********************************/
