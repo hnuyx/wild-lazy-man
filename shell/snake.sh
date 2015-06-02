f4cf7be9fc4614138d3b1a19ebd2091f8441413f
henuyx <365159110@qq.com>
2015-06-02 17:58:54 +0800

    snake.sh

--- shell/snake.sh
+++ shell/snake.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# author henuyx
 
 # hide cursor
 tput civis
 
 # clear and include line.sh
-clear
 source line.sh
+
+# snake pid
+snake_pid=$$
 
 # up/down/left/right key
 _UP_KEY='[A'
@@ -17,21 +20,64 @@ _RIGHT_KEY='[C'
 SnakeLen=10
 # current direction
 # 0 for up, 1 for down, 2 for left, 3 for right
-_UP_DIR=0
-_DOWN_DIR=1
-_LEFT_DIR=2
-_RIGHT_DIR=3
+_UP_DIR=20
+_DOWN_DIR=21
+_LEFT_DIR=22
+_RIGHT_DIR=23
 _CUR_DIR=$_RIGHT_DIR
 
 # SnakeBody[$SnakeHead] is head, SnakeBody[($SnakeHead + 1)%SnakeLen] is tail
 SnakeHead=`expr $SnakeLen - 1`
 SnakeBody=( [0]=0 1 2 3 4 5 6 7 8 9 )
+# mark map, 0  for nothing, 1 for Snake body, 2 for food
+_Mark_Nothing=0
+_Mark_SnakeBody=1
+_Mark_Food=2
+#_MarkMap=( [0]=1 1 1 1 1 1 1 1 1 1 )
+_MarkMap=( [0]=$_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody $_Mark_SnakeBody )
 
 # next position, default is right direction
 #echo "rows: $__rows cols $__cols"
 _MAX_POS=`expr $__rows \* $__cols`
 _NEXT_POS=`expr \( ${SnakeBody[$SnakeHead]} + 1 \) % $_MAX_POS`
 #echo "max pos: $_MAX_POS next pos: $_NEXT_POS"
+
+# save stty attr
+_STTY_G=`stty -g`
+
+
+game_over()
+{
+	echo ""
+	echo "What The Fuck Operation"
+	echo "Game Over"
+	# show cursor
+	tput cnorm
+
+	exit -1
+}
+
+game_exit()
+{
+	kill -9 $snake_pid
+	stty $_STTY_G
+	# show cursor
+	tput cnorm
+	clear
+	exit 0
+}
+trap "game_exit" INT TERM
+
+success()
+{
+	echo ""
+	echo "Well Done"
+	echo "Congratulations"
+
+	game_exit
+}
+
+#trap 'tput cnorm; exit 0' INT
 
 get_rc()
 {
@@ -59,56 +105,87 @@ get_tail_pos()
 	echo `expr \( $SnakeHead + 1 \) % $SnakeLen`
 }
 
-draw_next_pos()
+eat_food()
 {
-	# erase tail position
-	SnakeHead=`get_tail_pos`
-	_rc=`get_rc ${SnakeBody[$SnakeHead]}`
-	dot_space ${_rc[0]} ${_rc[1]}
+	# move left body
+	for((i=$SnakeLen;i>$SnakeHead;--i))
+	do
+		SnakeBody[i]=${SnakeBody[i - 1]}
+	done
 
-	# draw netx position
-	_rc=`get_rc $_NEXT_POS`
-	dot ${_rc[0]} ${_rc[1]} "#"
+	# insert a new position
+	SnakeLen=`expr $SnakeLen + 1`
+	SnakeHead=`get_tail_pos`
 	SnakeBody[$SnakeHead]=$_NEXT_POS
+
+	if [ "$SnakeLen" -eq "$_MAX_POS" ]; then
+		success
+	fi
 }
 
-# forever loop
-run()
+rand_food()
 {
+	# get no-body position
+	_rp=`expr $RANDOM % $_MAX_POS`
 	while :
 	do
-		# read key event
-		read -s -t 1 -n 3 key_ctrl
-		case $key_ctrl in
-		$_UP_KEY)
-			#echo "up key"
-			if [ "$_CUR_DIR" -ne "$_DOWN_DIR" ]; then
-				_CUR_DIR=$_UP_DIR
-			fi
-		;;
-		$_DOWN_KEY)
-			#echo "down key"
-			if [ "$_CUR_DIR" -ne "$_UP_DIR" ]; then
-				_CUR_DIR=$_DOWN_DIR
-			fi
-		;;
-		$_LEFT_KEY)
-			#echo "left key"
-			if [ "$_CUR_DIR" -ne "$_RIGHT_DIR" ]; then
-				_CUR_DIR=$_LEFT_DIR
-			fi
-		;;
-		$_RIGHT_KEY)
-			#echo "right key"
-			if [ "$_CUR_DIR" -ne "$_LEFT_DIR" ]; then
-				_CUR_DIR=$_RIGHT_DIR
-			fi
+		case ${_MarkMap[$_rp]} in
+		$_Mark_SnakeBody)
+			_rp=`expr $RANDOM % $_MAX_POS`
 		;;
 		*)
-			#echo "unknow key $key_ctrl"
+			break
 		;;
 		esac
 
+	done
+
+	# draw food
+	_rc=`get_rc $_rp`
+	dot ${_rc[0]} ${_rc[1]} "#"
+	_MarkMap[$_rp]=$_Mark_Food
+}
+
+draw_next_pos()
+{
+	# judge head hit itself
+	case ${_MarkMap[$_NEXT_POS]} in
+	$_Mark_SnakeBody)
+		game_over
+	;;
+	$_Mark_Food)
+		eat_food
+		rand_food
+	;;
+	*)
+		# erase tail position
+		SnakeHead=`get_tail_pos`
+		_rc=`get_rc ${SnakeBody[$SnakeHead]}`
+		dot_space ${_rc[0]} ${_rc[1]}
+		_MarkMap[${SnakeBody[$SnakeHead]}]=$_Mark_Nothing
+
+		# draw netx position
+		_rc=`get_rc $_NEXT_POS`
+		dot ${_rc[0]} ${_rc[1]} "#"
+		SnakeBody[$SnakeHead]=$_NEXT_POS
+		_MarkMap[${SnakeBody[$SnakeHead]}]=$_Mark_SnakeBody
+	;;
+	esac
+}
+
+live_snake()
+{
+	clear
+	init
+	rand_food
+
+	trap "_CUR_DIR=$_UP_DIR" $_UP_DIR
+	trap "_CUR_DIR=$_DOWN_DIR" $_DOWN_DIR
+	trap "_CUR_DIR=$_LEFT_DIR" $_LEFT_DIR
+	trap "_CUR_DIR=$_RIGHT_DIR" $_RIGHT_DIR
+
+	while :
+	do
 		case $_CUR_DIR in
 		$_UP_DIR)
 			_NEXT_POS=`expr \( ${SnakeBody[$SnakeHead]} + $_MAX_POS - $__cols \) % $_MAX_POS`
@@ -125,15 +202,61 @@ run()
 		esac
 
 		draw_next_pos
-		#usleep 200000
+		usleep 50000
+	done
+}
+
+control_dir()
+{
+	snake_pid=$1
+
+	while :
+	do
+		# read key event
+		read -s -n 3 key_ctrl
+		case $key_ctrl in
+		$_UP_KEY)
+			#echo "up key"
+			if [ "$_CUR_DIR" -ne "$_DOWN_DIR" ]; then
+				_CUR_DIR=$_UP_DIR
+				kill -$_CUR_DIR $snake_pid
+			fi
+		;;
+		$_DOWN_KEY)
+			#echo "down key"
+			if [ "$_CUR_DIR" -ne "$_UP_DIR" ]; then
+				_CUR_DIR=$_DOWN_DIR
+				kill -$_CUR_DIR $snake_pid
+			fi
+		;;
+		$_LEFT_KEY)
+			#echo "left key"
+			if [ "$_CUR_DIR" -ne "$_RIGHT_DIR" ]; then
+				_CUR_DIR=$_LEFT_DIR
+				kill -$_CUR_DIR $snake_pid
+			fi
+		;;
+		$_RIGHT_KEY)
+			#echo "right key"
+			if [ "$_CUR_DIR" -ne "$_LEFT_DIR" ]; then
+				_CUR_DIR=$_RIGHT_DIR
+				kill -$_CUR_DIR $snake_pid
+			fi
+		;;
+		*)
+			#echo "unknow key $key_ctrl"
+		;;
+		esac
 	done
 }
 
 # main
-init
-run
+
+if [ "$1" == "moving" ]; then
+	live_snake
+else
+	bash $0 moving &
+	control_dir $! 2>/dev/null
+fi
 
 clear
-
-# show cursor
-tput cnorm
