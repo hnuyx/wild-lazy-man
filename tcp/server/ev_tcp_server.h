0a6d8ea4cc1eb59eb319e2e5fae3c2516a33bb75
Henry Liu <365159110@qq.com>
2016-09-08 19:03:42 +0800

    creat ev_tcp_server.h

---
+++ tcp/server/ev_tcp_server.h
@@ -0,0 +1,29 @@
+/* File Name: ev_tcp_server.h
+ *
+ * Description: tcp server written by libevent
+ *
+ * Created by henuyx on 2016-09-07
+ *
+ */
+
+#ifndef __EV_TCP_SERVER_H__
+#define __EV_TCP_SERVER_H__
+
+/*******************INCLUDE*******************************/
+
+/*******************GLOBAL********************************/
+
+class EvTcpServer
+{
+public:
+	EvTcpServer();
+	virtual ~EvTcpServer();
+
+protected:
+
+privated:
+};
+
+#endif
+
+/*******************EOF***********************************/
